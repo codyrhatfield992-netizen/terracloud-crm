662d704cf2b48907d5dd75bbf6da868ddb76ef1a
codyrhatfield992-netizen <codyrhatfield992@gmail.com>
2026-04-29 23:36:57 -0400

    Redesign Terra Cloud XR interface

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -4,11 +4,11 @@ import TopBar from "./TopBar";
 
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen flex">
+    <div className="min-h-screen flex xr-shell">
       <AppSidebar />
       <div className="flex-1 ml-64 flex flex-col">
         <TopBar />
-        <main className="flex-1 p-8 overflow-y-auto scrollbar-thin">
+        <main className="flex-1 p-7 2xl:p-9 overflow-y-auto scrollbar-thin xr-command-surface">
           {children}
         </main>
       </div>
